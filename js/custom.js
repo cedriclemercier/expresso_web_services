84699af7735b61db76c120d7955a8357ab561280
cedric@gmail.com <cedric.lemercierlaos@gmail.com>
2019-03-19 16:29:17 +1100

    test

--- js/custom.js
+++ js/custom.js
@@ -254,5 +254,15 @@ function fixStepIndicator(n) {
 
 
 // Add active class to header links
-
+$(document).ready(function () {   
+            $(function($) {
+ let url = window.location.href;
+  $('.navbar-nav li a').each(function() {
+   if (this.href === url) {
+   $(this).addClass('active');
+  }
+ });
+});
+            
+    });
                  
