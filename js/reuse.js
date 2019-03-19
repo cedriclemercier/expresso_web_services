ec5e9d1db8b46815e5a0f4ebe306b2de88bc72f1
cedric@gmail.com <cedric.lemercierlaos@gmail.com>
2019-03-19 16:36:23 +1100

    Update reuse.js

--- js/reuse.js
+++ js/reuse.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
     $("#navheader").load("header.html");
+    
+    $(function(){
+    var current = location.pathname;
+    $(' li a').each(function(){
+        var $this = $(this);
+        // if the current path is like this link, make it active
+        if($this.attr('href').indexOf(current) !== -1){
+            $this.addClass('active');
+        }
+    })
+});
 });
