0c17f469cfbca495b294b7e886b12e4a78c32770
cedric@gmail.com <cedric.lemercierlaos@gmail.com>
2019-03-19 16:44:38 +1100

    Add back fixed header nav for main pages

--- js/reuse.js
+++ js/reuse.js
@@ -1,18 +1,4 @@
 $(document).ready(function() {
-    $(function() {
-    var current = location.pathname;
-    $('ul li a').each(function() {
-        var $this = $(this); 
-
-        // we check comparison between current page and attribute redirection.
-        if ($this.attr('href') === current) {
-            $this.addClass('active');
-        }
-    });
-});
-});
-
-$(document).ready(function() {
     $("#navheader").load("header.html");
 });
 
