1c6f71fd771a02abb3eba1ffacc0fe6ac1a74ab8
cedric@gmail.com <cedric.lemercierlaos@gmail.com>
2019-04-05 19:02:07 +1100

    fade in and out contact form

--- js/custom.js
+++ js/custom.js
@@ -152,10 +152,11 @@ $(document).ready(function () {
                         $('#contact-form :input').attr('disabled', 'disabled');
                         $('#contact-form').fadeTo("slow", 1, function () {
                             $(this).find(':input').attr('disabled', 'disabled');
-                            $(this).find('label').css('cursor', 'default');
-                            $('#success').fadeIn();
+/*                            $(this).find('label').css('cursor', 'default');
+                            $('#success').fadeIn();*/
                         });
                          $('#contact-form').fadeOut();
+                        $('.success-message').fadeIn();
                         /*
                         document.getElementById('contact-form').style.display = "none";
                         
